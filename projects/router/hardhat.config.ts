e5ae75c52d94c3e529256ac5abe94e2a9981a253
gurujowa <gurujowa@gmail.com>
2024-02-19 10:19:40 +0000

    deploy router

--- projects/router/hardhat.config.ts
+++ projects/router/hardhat.config.ts
@@ -10,24 +10,6 @@ import '@nomiclabs/hardhat-etherscan'
 import 'solidity-docgen'
 require('dotenv').config({ path: require('find-config')('.env') })
 
-// const bscTestnet: NetworkUserConfig = {
-//   url: 'https://rpc.ankr.com/bsc_testnet_chapel',
-//   chainId: 97,
-//   accounts: [process.env.KEY_TESTNET!],
-// }
-
-// const goerli: NetworkUserConfig = {
-//   url: `https://eth-goerli.g.alchemy.com/v2/${process.env.GOERLI_API_KEY}`,
-//   chainId: 5,
-//   // accounts: [process.env.KEY_GOERLI!],
-// }
-
-// const bscMainnet: NetworkUserConfig = {
-//   url: 'https://bsc-dataseed.binance.org/',
-//   chainId: 56,
-//   // accounts: [process.env.KEY_MAINNET!],
-// }
-
 const bscTestnet: NetworkUserConfig = {
   url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
   chainId: 97,
@@ -40,19 +22,13 @@ const bscMainnet: NetworkUserConfig = {
   accounts: [process.env.KEY_MAINNET!],
 }
 
-const goerli: NetworkUserConfig = {
-  url: 'https://rpc.ankr.com/eth_goerli',
-  chainId: 5,
-  accounts: [process.env.KEY_GOERLI!],
-}
-
 const eth: NetworkUserConfig = {
   url: 'https://eth.llamarpc.com',
   chainId: 1,
   accounts: [process.env.KEY_ETH!],
 }
 
-const zkkatana: NetworkUserConfig = {
+const zkatana: NetworkUserConfig = {
   url: 'https://rpc.startale.com/zkatana/',
   chainId: 1261120,
   accounts: [process.env.KEY_TESTNET!],
@@ -68,7 +44,7 @@ const config: HardhatUserConfig = {
     },
     ...(process.env.KEY_TESTNET && { bscTestnet }),
     ...(process.env.KEY_MAINNET && { bscMainnet }),
-    ...(process.env.KEY_TESTNET && { zkkatana }),
+    ...(process.env.KEY_TESTNET && { zkatana }),
     ...(process.env.KEY_ETH && { eth }),
     // goerli: goerli,
     // mainnet: bscMainnet,
--- projects/router/scripts/deploy2.ts
+++ projects/router/scripts/deploy2.ts
@@ -1,6 +1,5 @@
 import { ethers, network } from 'hardhat'
 import { configs } from '@pancakeswap/common/config'
-import { tryVerify } from '@pancakeswap/common/verify'
 import { writeFileSync } from 'fs'
 
 async function main() {
@@ -11,19 +10,18 @@ async function main() {
     throw new Error(`No config found for network ${networkName}`)
   }
 
-  const v3DeployedContracts = await import(`@pancakeswap/v3-core/deployments/${networkName}.json`)
-  const v3PeripheryDeployedContracts = await import(`@pancakeswap/v3-periphery/deployments/${networkName}.json`)
+  const v3DeployedContracts = await import(`@verylongswap/core/deployments/${networkName}.json`)
+  console.log('v3DeployedContracts', v3DeployedContracts, networkName)
 
-  const pancakeV3PoolDeployer_address = v3DeployedContracts.PancakeV3PoolDeployer
-  const pancakeV3Factory_address = v3DeployedContracts.PancakeV3Factory
-  const positionManager_address = v3PeripheryDeployedContracts.NonfungiblePositionManager
+  const pancakeV3PoolDeployer_address = v3DeployedContracts.VeryLongPoolDeployer
+  const pancakeV3Factory_address = v3DeployedContracts.VeryLongFactory
+  const positionManager_address = "0xc076DED86B7Cb89f4830Ce98543769c47B4290c5"
 
   /** SmartRouterHelper */
   console.log('Deploying SmartRouterHelper...')
   const SmartRouterHelper = await ethers.getContractFactory('SmartRouterHelper')
   const smartRouterHelper = await SmartRouterHelper.deploy()
   console.log('SmartRouterHelper deployed to:', smartRouterHelper.address)
-  // await tryVerify(smartRouterHelper)
 
   /** SmartRouter */
   console.log('Deploying SmartRouter...')
@@ -43,15 +41,6 @@ async function main() {
   )
   console.log('SmartRouter deployed to:', smartRouter.address)
 
-  // await tryVerify(smartRouter, [
-  //   config.v2Factory,
-  //   pancakeV3PoolDeployer_address,
-  //   pancakeV3Factory_address,
-  //   positionManager_address,
-  //   config.stableFactory,
-  //   config.stableInfo,
-  //   config.WNATIVE,
-  // ])
 
   /** MixedRouteQuoterV1 */
   const MixedRouteQuoterV1 = await ethers.getContractFactory('MixedRouteQuoterV1', {
@@ -67,14 +56,6 @@ async function main() {
     config.WNATIVE
   )
   console.log('MixedRouteQuoterV1 deployed to:', mixedRouteQuoterV1.address)
-
-  // await tryVerify(mixedRouteQuoterV1, [
-  //   pancakeV3PoolDeployer_address,
-  //   pancakeV3Factory_address,
-  //   config.v2Factory,
-  //   config.stableFactory,
-  //   config.WNATIVE,
-  // ])
 
   /** QuoterV2 */
   const QuoterV2 = await ethers.getContractFactory('QuoterV2', {
@@ -95,8 +76,6 @@ async function main() {
   })
   const tokenValidator = await TokenValidator.deploy(config.v2Factory, positionManager_address)
   console.log('TokenValidator deployed to:', tokenValidator.address)
-
-  // await tryVerify(tokenValidator, [config.v2Factory, positionManager_address])
 
   const contracts = {
     SmartRouter: smartRouter.address,
